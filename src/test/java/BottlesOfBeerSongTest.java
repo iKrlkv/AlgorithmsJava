84ab0a4f277c1cbbdf3b254a624d2f935cd72cbd
regent88 <il9.korolkov@gmail.com>
2021-10-31 23:11:17 +0500

    BottlesOfBeerSongTest added

--- src/test/java/BottlesOfBeerSongTest.java
+++ src/test/java/BottlesOfBeerSongTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -35,8 +36,10 @@ public class BottlesOfBeerSongTest {
 
         List<WebElement> songText = driver.findElements(By.xpath("//div[@id='main']//p"));
         for (WebElement item : songText) {
-            sb.append(item.getText()).append("\n");
+            sb.append(item.getText()).append("\n\n");
         }
-        System.out.println(sb.toString());
+        sb.deleteCharAt(sb.length() - 1);
+
+        Assert.assertEquals(BottlesOfBeerSongWithSB.getSongText().toString(), sb.toString());
     }
 }
