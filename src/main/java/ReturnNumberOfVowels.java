7544cfb72567168c672c4f96929f14bbb5daae6e
regent88 <il9.korolkov@gmail.com>
2021-11-02 22:36:16 +0500

    ReturnNumberOfVowels class and test for it added

---
+++ src/main/java/ReturnNumberOfVowels.java
@@ -0,0 +1,29 @@
+public class ReturnNumberOfVowels {
+
+    //Написать метод, который посчитает количество гласных букв в строке.
+    //Для нахождения гласных букв использовать метод contains()
+    //(гласные буквы - “a”, “e”, “i”, “o”, “u”)
+
+    public static boolean isSubstringVowel(int i, int j, String str) {
+        String s = str.substring(i, j).toLowerCase();
+        return s.contains("a") || s.contains("e") || s.contains("i") || s.contains("o") || s.contains("u");
+    }
+
+    public static int returnNumberOfVowels(String str) {
+
+        int cnt = 0;
+        for (int i = 0, j = 1; i < str.length(); i++, j++) {
+            if (j == str.length()) {
+                if (isSubstringVowel(i, j, str)) {
+                    cnt++;
+                }
+                break;
+            } else {
+                if (isSubstringVowel(i, j, str)) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
---
+++ src/test/java/ReturnNumberOfVowelsTest.java
@@ -0,0 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReturnNumberOfVowelsTest {
+
+    //Test Data:
+    //“QA For Everyone” -> 6
+    //“Java lessons are fun” -> 7
+
+    @Test
+    public void ReturnNumberOfVowels() {
+
+        String testData1 = "QA For Everyone";
+        String testData2 = "Java lessons are fun";
+        int expectedResult1 = 6;
+        int expectedResult2 = 7;
+
+        Assert.assertEquals(expectedResult1, ReturnNumberOfVowels.returnNumberOfVowels(testData1));
+        Assert.assertEquals(expectedResult2, ReturnNumberOfVowels.returnNumberOfVowels(testData2));
+    }
+}
